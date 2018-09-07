4f58bb6fec6fe00ae6431f373b73f6887f44ae3e
Administrator <Administrator@USER-20180616KD>
2018-09-07 14:50:51 +0800

    1

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 //表示该controller类下所有的方法都公用的一级上下文根
-@RequestMapping(value = "/springboot")
+@RequestMapping(value = "/demo")
 public class UserController {
     //这里使用@RequestMapping注解表示该方法对应的二级上下文路径
-    @RequestMapping(value = "/getUserByGet", method = RequestMethod.GET)
-    String getUserByGet(){
-        return "Hello ";
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    String index(){
+        return "index.html";
     }
 }
