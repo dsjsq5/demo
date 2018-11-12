fb8478841a1367c9e7493d55dc4c38ad3b041ce3
dsjsq5 <595808280@qq.com>
2018-11-12 16:16:30 +0800

    mysql/mybatis/shiro

---
+++ src/main/resources/static/static/commonjs.js
@@ -0,0 +1,5 @@
+document.write('<script src="../static/vue.js"  type="text/javascript"></script>');
+document.write('<script src="../static/element.js"  type="text/javascript"></script>');
+
+document.write('<link href="../static/element.css" rel="stylesheet" type="text/css">');
+document.write('<link href="../static/inputcss.css" rel="stylesheet" type="text/css">');
